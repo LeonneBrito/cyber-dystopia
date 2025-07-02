9e98b5c673f8e046ac3b904da8e16a49f48054ac
LeonneBrito <britoleonne@gmail.com>
2025-07-02 01:47:00 -0300

    feat: initial commit

---
+++ src/constants/pricing.ts
@@ -0,0 +1,10 @@
+export const pricing = {
+  COLETE: {
+    ally: 2000,
+    nonAlly: 2500,
+  },
+  TROJAN: {
+    ally: 3000,
+    nonAlly: 3500,
+  },
+}
---
+++ src/constants/recipes.ts
@@ -0,0 +1,12 @@
+export const recipes = {
+  COLETE: {
+    aluminum: 20,
+    rubber: 20,
+    clothes: 2,
+  },
+  TROJAN: {
+    aluminum: 50,
+    rubber: 50,
+    clothes: 0,
+  },
+}
